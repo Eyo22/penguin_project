bbbed42444e7fc354269443c940352f5add5a3f7
Eyo22 <eyo.alexander@gmail.com>
2023-09-27 14:58:33 +0100

    firstcommit

---
+++ Palmer_penguins.R
@@ -0,0 +1,19 @@
+#install necessary package
+install.packages("tidyverse")
+#load the library
+library(dplyr)
+#load the penguin data into R
+penguins <- read.table("data/penguin_data.txt", header = T)
+#view part of the data
+glimpse(penguins)
+#run a linear regression
+model1 <- lm(body_mass_g ~ flipper_length_mm, data = penguins)
+summary(model1)
+#create a nice plot in ggplot2
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, colour = species)) + geom_point() + stat_smooth(method = "lm") 
+#save plot in figs folder
+ggsave("figs/1_flipper_bodymass_regression.png")
+#subset the data to female penguins only
+penguins_female <- subset(penguins, sex == "female")
+#save the edited dataset
+write.table(penguins_female, "results/1_penguin_female_only.txt", quote = F, col.names = F, sep= "\t")
