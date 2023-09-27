84fdaf5f0067d42cfc1e0753ab4a26b1eb4487f8
Eyo22 <eyo.alexander@gmail.com>
2023-09-27 15:26:41 +0100

    I can commit multiple modified versions

--- Palmer_penguins.R
+++ Palmer_penguins.R
@@ -10,10 +10,15 @@ glimpse(penguins)
 model1 <- lm(body_mass_g ~ flipper_length_mm, data = penguins)
 summary(model1)
 #create a nice plot in ggplot2
-ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, colour = species)) + geom_point() + stat_smooth(method = "lm") 
+#ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, colour = species)) + geom_point() + stat_smooth(method = "lm") 
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, colour = species)) + geom_point() + stat_smooth(method = "lm")
 #save plot in figs folder
 ggsave("figs/1_flipper_bodymass_regression.png")
 #subset the data to female penguins only
 penguins_female <- subset(penguins, sex == "female")
 #save the edited dataset
 write.table(penguins_female, "results/1_penguin_female_only.txt", quote = F, col.names = F, sep= "\t")
+
+#git config --global user.email "eyo.alexander@gmail.com"
+#git config --global user.name "Eyo22"
+#I can use git
